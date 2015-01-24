b0991f032453ef789fbbed65152ba94ff5ca274b
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-01-25 05:42:18 +0700

    author name must be UNIQUE

--- data/authors.sql
+++ data/authors.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS authors (
     id SERIAL PRIMARY KEY,
     avatar_url text,
-    name varchar(50) NOT NULL,
+    name varchar(50) UNIQUE NOT NULL,
     company_name varchar(50),
     twitter_username varchar(15)
 );
