94be5c12fb77de345ecc181e3db4cd4242e29327
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-01-25 08:15:11 +0700

    add unique constraint on post_id

--- data/quotes.sql
+++ data/quotes.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS quotes (
     id SERIAL PRIMARY KEY,
     author_id integer REFERENCES authors(id),
-    post_id text NOT NULL,
+    post_id text UNIQUE NOT NULL,
     content text NOT NULL,
     permalink text NOT NULL,
     picture_url text NOT NULL
